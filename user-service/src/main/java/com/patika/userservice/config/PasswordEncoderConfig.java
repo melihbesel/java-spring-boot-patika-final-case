048adce42d07ca12a373558f19dfb90313f8d379
melihbesel <mbesel2005@gmail.com>
2024-08-27 02:40:43 +0300

    UPDATE PasswordEncoderConfig

--- user-service/src/main/java/com/patika/userservice/config/PasswordEncoderConfig.java
+++ user-service/src/main/java/com/patika/userservice/config/PasswordEncoderConfig.java
@@ -2,7 +2,6 @@ package com.patika.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -10,6 +9,6 @@ public class PasswordEncoderConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new Sha512PasswordEncoder();
     }
 }
